16032c9fbd5232f3bec2fbd5a2d92b39f58164d3
yudonghui <yudonghui@51caixiang.com>
2017-02-24 15:13:04 +0800

    初学js交互

---
+++ app/src/main/java/com/daxiang/jsandroid/MainActivity.java
@@ -0,0 +1,81 @@
+package com.daxiang.jsandroid;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.WindowManager;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String URL = "file:///android_asset/helloworld.html";
+    private WebView webView;
+    public String tag = "MainActivity";
+    private Context mContext;
+
+    @SuppressLint("JavascriptInterface")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        // 进行全屏
+
+        mContext = this;
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        webView = (WebView) this.findViewById(R.id.wv);
+
+        webView.loadUrl(URL);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new JSHook(), "hello");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Log.d(tag, " url:" + url);
+                view.loadUrl(url);// 当打开新链接时，使用当前的 WebView，不会使用系统其他浏览器
+                return true;
+            }
+        });
+    }
+
+    public class JSHook {
+        @JavascriptInterface
+        public void javaMethod(String p) {
+            Log.e(tag, "JSHook.JavaMethod() called! + " + p);
+        }
+
+        @JavascriptInterface
+        public void showAndroid() {
+            final String info = "来自手机内的内容！！！";
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    webView.loadUrl("javascript:show('" + info + "')");
+                }
+            });
+        }
+
+        public String getInfo() {
+            return "获取手机内的信息！！";
+        }
+    }
+
+    @Override
+    //设置回退
+    //覆盖Activity类的onKeyDown(int keyCoder,KeyEvent event)方法
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack(); //goBack()表示返回WebView的上一页面
+            this.finish();
+            return true;
+        }
+        return false;
+    }
+}
